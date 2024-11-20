14774ab789f56527ae239271b034caa57d3f678b
Hooriya M. Fareed <parih3080@gmail.com>
2024-11-20 16:39:58 +0500

    push_changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,20 +1,21 @@
 export const fetchProducts = async () => {
   try {
-    const response = await fetch(" https://api.escuelajs.co/api/v1/products", {
-      cache: "force-cache",
-    });
+    const response = await fetch(
+      "https://dummyjson.com/products?limit=120",
+      {}
+    );
     if (!response.ok) {
       throw new Error("Failed to fetch products");
     }
     const data = await response.json();
-    const transformedData = data.map((item: any) => ({
+    const transformedData = data.products.map((item: any) => ({
       id: item.id,
       title: item.title,
       description: item.description,
       images: item.images,
       price: item.price,
     }));
-    return transformedData.slice(4, 48);
+    return transformedData;
   } catch (error) {
     console.error("Error fetching products:", error);
     return [];
@@ -22,9 +23,7 @@ export const fetchProducts = async () => {
 };
 
 export async function getProduct(id: string) {
-  const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`, {
-    cache: "force-cache",
-  });
+  const res = await fetch(`https://dummyjson.com/products/${id}`, {});
   if (!res.ok) throw new Error("Failed to fetch product");
   return res.json();
 }
